17f7150b67278e488ddd80467832d7fe084c80b4
fanglingfeng <954410925@qq.com>
2018-07-19 20:09:22 +0800

    list

--- baoan/pages/gov/person/person.js
+++ baoan/pages/gov/person/person.js
@@ -7,7 +7,13 @@ Page({
   data: {
       isTheme:true
   },
-
+  showTheme:function(){
+    this.setData({isTheme:true});
+    
+  },
+  showDepartment:function(){
+    this.setData({isTheme:false});
+  },
   /**
    * 生命周期函数--监听页面加载
    */
