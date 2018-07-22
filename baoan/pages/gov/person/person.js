5762d926b3c41b008777801cd76bfa94c8a48bc6
fanglingfeng <954410925@qq.com>
2018-07-22 16:58:37 +0800

    url

--- baoan/pages/gov/person/person.js
+++ baoan/pages/gov/person/person.js
@@ -1,72 +1,102 @@
 // pages/gov/person/person.js
+var surl = require('../../../utils/surl.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-      isTheme:true
+    dataTheme: [],
+    isTheme: true
   },
-  showTheme:function(){
-    this.setData({isTheme:true});
-    
+  init: function (e) {
+    var that = this;
+    wx.request({
+      url: surl.host + 'weigov/businessController.do?getPictureByID&ID=440306&PAGENO=1&PAGESIZE=1000&SFYDSB=1',
+      success: function (message) {
+        console.log(message);
+        var data = message.data.obj;
+        try {
+          data = JSON.parse(data).ReturnValue;
+          for (var i = 0; i < data.length; i++) {
+            var index = data[i].PICTUREPATH.lastIndexOf("\/");
+            data[i].PICTUREPATH = "http://203.91.37.98:8083" + data[i].PICTUREPATH.substring(0, index) + "/new/" + data[i].PICTUREPATH.substring(index + 1, data[i].PICTUREPATH.length);
+          }
+          wx.setStorageSync('dataTheme', data);
+          console.log(data);
+          that.setData({
+            dataTheme: data
+          })
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    })
   },
-  showDepartment:function(){
-    this.setData({isTheme:false});
+  showTheme: function() {
+    this.setData({
+      isTheme: true
+    });
+
+  },
+  showDepartment: function() {
+    this.setData({
+      isTheme: false
+    });
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
---
+++ baoan/utils/surl.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //  host:"http://localhost:8080/"
+    // host:"https://weigov.baoan.gov.cn/weigov/",
+  host: "http://weigov.baoan.gov.cn/weigov/",
+    fileHost:"http://203.91.37.98:8083/"
+};
